24b0a9f3ab4c0641c860daa66f6bf3648b645799
YOUR NAME <YOUR EMAIL@SAMLECOM>
2025-09-25 21:02:20 +0700

    Implementasi authentication dengan Laravel Breeze + proteksi route

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,37 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MahasiswaController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Semua route project.
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-Route::resource('mahasiswa', MahasiswaController::class);
-Route::get('mahasiswa-trash', [MahasiswaController::class, 'trash'])->name('mahasiswa.trash');
-Route::get('mahasiswa-restore/{npm}', [MahasiswaController::class, 'restore'])->name('mahasiswa.restore');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
+    Route::get('/mahasiswa/create', [MahasiswaController::class, 'create'])->name('mahasiswa.create');
+    Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.store');
+    Route::get('/mahasiswa/{id}/edit', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
+    Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
+    Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.destroy');
+    Route::get('/mahasiswa/restore/{id}', [MahasiswaController::class, 'restore'])->name('mahasiswa.restore');
+});
+
+require __DIR__ . '/auth.php';
